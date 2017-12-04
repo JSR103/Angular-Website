34a28e90a94ec389198246a734deb9d2666c2b25
JSR103 <silva.joel.r@gmail.com>
2017-12-04 13:32:05 -0800

    Took out the logic that parses the variable from the input

--- js/controllers/CalculatorController.js
+++ js/controllers/CalculatorController.js
@@ -1,9 +1,6 @@
 app.controller('CalculatorController', function($scope) {
             $scope.result = function() {
 				
-				$scope.a = parseInt($scope.a, 10);
-				$scope.b = parseInt($scope.b, 10);
-				
                 if ($scope.operator == '+') {
                     return $scope.a + $scope.b;
                 }
