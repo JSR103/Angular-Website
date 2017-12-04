3f6322f726d1b36ddee954733f8ac436b4efe8e3
JSR103 <silva.joel.r@gmail.com>
2017-12-04 12:20:51 -0800

    Made the logic parse the whats in the input boxes for the simple calculator

--- js/controllers/CalculatorController.js
+++ js/controllers/CalculatorController.js
@@ -1,5 +1,9 @@
 app.controller('CalculatorController', function($scope) {
             $scope.result = function() {
+				
+				$scope.a = parseInt($scope.a, 10);
+				$scope.b = parseInt($scope.b, 10);
+				
                 if ($scope.operator == '+') {
                     return $scope.a + $scope.b;
                 }
