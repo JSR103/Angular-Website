7eaa2f0738151ba4e88af1914ba6f9c5d5180520
JSR103 <silva.joel.r@gmail.com>
2017-12-04 13:01:24 -0800

    add a porperty called link and it well..a link

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,25 +4,29 @@ app.controller('MainController', ['$scope', function($scope) {
 		icon: './img/crunchyroll.png', 
     	title: 'Crunchyroll', 
     	developer: 'Ellation', 
-    	price: 0.99 
+    	price: 0.99,
+		link: 'http://www.crunchyroll.com/'
   	}, 
  	{ 
 		icon: './img/hulu.png', 
     	title: 'Hulu', 
     	developer: 'Hulu LLC', 
-    	price: 2.99 
+    	price: 2.99,
+		link: 'https://www.hulu.com'
   	}, 
  	{ 
 		icon: './img/netflix.png', 
     	title: 'Netflix', 
     	developer: ' Netflix', 
-    	price: 1.99 
+    	price: 1.99,
+		link: 'https://www.netflix.com/'
   	},  
  	{
 		icon: './img/vrv.jpg', 
         title: 'VRV',
         developer: 'Ellation',
-        price: 1.99
+        price: 1.99,
+		link: 'https://vrv.co/'
     } 
 	]
 }]);
