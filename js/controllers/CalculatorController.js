e6e8fad2a2017a0b9ca92de99bee10c0795124f2
JSR103 <silva.joel.r@gmail.com>
2017-12-03 19:43:18 -0800

    So I added this controller to my module. It controls the logic of the calculator

---
+++ js/controllers/CalculatorController.js
@@ -0,0 +1,16 @@
+app.controller('CalculatorController', function($scope) {
+            $scope.result = function() {
+                if ($scope.operator == '+') {
+                    return $scope.a + $scope.b;
+                }
+                if ($scope.operator == '-') {
+                    return $scope.a - $scope.b;
+                }
+                if ($scope.operator == '*') {
+                    return $scope.a * $scope.b;
+                }
+                if ($scope.operator == '/') {
+                    return $scope.a / $scope.b;
+                }
+            };
+        });
